019d256f6479aa850dc597051afa1df753129c08
JacobNicLarsen <JacobNicLarse@gmail.com>
2018-12-09 00:04:47 +0100

    Oppgave 4

--- src/Oblig1/Oblig1.java
+++ src/Oblig1/Oblig1.java
@@ -1,11 +1,16 @@
 package Oblig1;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Oblig1 {
     public static void main(String[] args) {
         int a[] = {1,2,54,1,123,5,43,1,234,5,61};
 
-        System.out.println(maks(a));
+        System.out.println(ombyttinger(a));
     }
+
+
 
     public static void bytt(int[] a, int i, int j){
         int temp = a[i];
@@ -22,5 +27,18 @@ public class Oblig1 {
             }
         }
         return a[a.length-1];
+    }
+
+    public static int ombyttinger(int[]  a){
+        if (a.length == 0) throw new IllegalArgumentException("Kan ikke ha en tom array");
+
+        int ombyttinger = 0;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i - 1] > a[i]){
+                bytt(a,i,i - 1);
+                ombyttinger++;
+            }
+        }
+        return ombyttinger;
     }
 }
